94178b3deb9276438e5cb164ca097fe59bc1e5f7
M Farhan <farhanmahesa10@gmail.com>
2024-05-13 13:28:03 +0700

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainLayout from "../components/layouts/MainLayout";
 import {
   HomeFAQ,
@@ -10,8 +10,20 @@ import {
   HomeOurClient,
   HomeSales,
 } from "../components/organisms";
+import axios from "axios";
 
 const Home = () => {
+  const init = async () => {
+    try {
+      const result = await axios.get("https://pm-api.flexioncloud.com:8501");
+      console.log("res-good", result);
+    } catch (error) {
+      console.log("res-err", error);
+    }
+  };
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <MainLayout activePage="beranda">
       <HomeHero />
